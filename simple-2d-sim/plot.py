f82493f29adfdf4f879ccd04e334218a3d375eb7
Andreas Karlsson <andreask413@gmail.com>
2023-02-18 21:01:00 +0100

    Removed unused lines

--- simple-2d-sim/plot.py
+++ simple-2d-sim/plot.py
@@ -1,9 +1,7 @@
 import numpy as np
 import random
 
-
 import matplotlib.pyplot as plt
-
 
 from sim import SimulationState, SimulationParameters, ControlSignals, Simulator
 from regulator import Regulator, LookaheadSpeedRegulator, NullRegulator
