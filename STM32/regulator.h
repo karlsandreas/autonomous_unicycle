cc56a198570f0713c84c459640b6abe8d0260993
Andreas Karlsson <andreask413@gmail.com>
2023-02-27 21:06:44 +0100

    Working regulator in C

---
+++ STM32/regulator.h
@@ -0,0 +1,6 @@
+
+#define param_A -1.6492579658751876F
+#define param_B -0.48285301954085175F
+#define param_C 21.84620564928607F
+#define param_D 2.824481941982574F
+#define param_E 2.0854114637561922F
